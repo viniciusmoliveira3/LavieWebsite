0e8daa035a27dfd70ef0bd3138cc92d98ddf62dd
Vinicius Mendonça de Oliveira <viniciusmoliveira3@gmail.com>
2024-03-25 14:39:29 -0300

    Primeiro commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LavieWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
